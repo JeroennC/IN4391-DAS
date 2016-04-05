32d3c1771a20a4cc8d8788cb7ecf2ffad51b3be5
Jeroen Castelein <jeroencastelein11@gmail.com>
2016-04-05 15:00:31 +0200

    Update exec

--- src/das/server/Server.java
+++ src/das/server/Server.java
@@ -346,6 +346,7 @@ public class Server extends Node {
 		Address addr = m.getFromAddress();
 		try {
 			addr.setAddress(getClientHost());
+			System.out.println(addr.getAddress());
 		} catch (ServerNotActiveException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
